485d1d3292cead462ba2fc36e85a53f5176906c6
avinashkvar <k3076183@gmail.com>
2022-12-08 01:00:42 +0530

    navbar-fixed

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -3,48 +3,7 @@ import {Text} from '@chakra-ui/react'
 export const Skills = () => {
 	return (
 		<>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
-			<Text>
-				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam
-				odio harum maxime mollitia assumenda asperiores, minus autem dicta
-				deleniti? Impedit nam, aspernatur ratione ea quia ducimus fugit
-				quaerat molestiae est.
-			</Text>
+			
 		</>
 	);
 };
