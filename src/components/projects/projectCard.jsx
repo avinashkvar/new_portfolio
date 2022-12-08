522d015e1436b4be4bd74677f0622bf7dcc1bc2f
avinashkvar <k3076183@gmail.com>
2022-12-08 22:54:17 +0530

    projects 95%

--- src/components/projects/projectCard.jsx
+++ src/components/projects/projectCard.jsx
@@ -16,6 +16,7 @@ import { useState } from 'react';
 import { FaCode } from 'react-icons/fa';
 import { IoIosEye } from 'react-icons/io';
 import '../navbar/link.css';
+import { Zoom } from 'react-awesome-reveal';
 
 export const ProjectCard = ({
 	image,
@@ -29,53 +30,61 @@ export const ProjectCard = ({
 	const bg = useColorModeValue('#2d3748', '#f7fafc');
 	const color = useColorModeValue('#f7fafc', '#2d3748');
 	return (
-		<Card maxW={['sm', 'md', 'lg']} align="center">
-			<CardHeader
-				align="center"
-				onMouseEnter={() => setIsEntered(true)}
-				onMouseLeave={() => setIsEntered(false)}
-			>
-				<Image src={image}></Image>
-				<Heading fontSize={['15px', '18px', '24px']} fontWeight="bold">
-					{title}
-				</Heading>
-				<Text>{title2}</Text>
-			</CardHeader>
-
-			<CardBody>
-				<Collapse in={isEntered} animateOpacity>
-					<Text>{discription}</Text>
-				</Collapse>
-			</CardBody>
-
-			<CardFooter>
-				<Button
-					margin="0px 20px 0px 20px"
-					width="150px"
-					height="auto"
-					padding={['10px', '13px', '15px']}
-					_hover={{ background: bg, color: color }}
+		<Zoom>
+			<Card maxW={['sm', 'md', 'lg']} align="center">
+				<CardHeader
+					align="center"
+					onMouseEnter={() => setIsEntered(true)}
+					onMouseLeave={() => setIsEntered(false)}
 				>
-					<Link className="link" href={live} isExternal>
-						<Text fontSize={['18px', '18px', '20px']} fontWeight="bold">
-							Live<Icon as={IoIosEye} marginLeft="5px"></Icon>
-						</Text>
-					</Link>
-				</Button>
-				<Button
-					margin="0px 20px 0px 20px"
-					width="150px"
-					height="auto"
-					padding={['10px', '13px', '15px']}
-					_hover={{ background: bg, color: color }}
-				>
-					<Link className="link" isExternal href={gitHub}>
-						<Text fontSize={['18px', '18px', '20px']} fontWeight="bold">
-							Code<Icon as={FaCode} marginLeft="5px"></Icon>
-						</Text>
-					</Link>
-				</Button>
-			</CardFooter>
-		</Card>
+					<Image src={image}></Image>
+					<Heading fontSize={['15px', '18px', '24px']} fontWeight="bold">
+						{title}
+					</Heading>
+					<Text>{title2}</Text>
+				</CardHeader>
+
+				<CardBody>
+					<Collapse in={isEntered} animateOpacity>
+						<Text>{discription}</Text>
+					</Collapse>
+				</CardBody>
+
+				<CardFooter>
+					<Button
+						margin="0px 20px 0px 20px"
+						width="150px"
+						height="auto"
+						padding={['10px', '13px', '15px']}
+						_hover={{ background: bg, color: color }}
+					>
+						<Link className="link" href={live} isExternal>
+							<Text
+								fontSize={['18px', '18px', '20px']}
+								fontWeight="bold"
+							>
+								Live<Icon as={IoIosEye} marginLeft="5px"></Icon>
+							</Text>
+						</Link>
+					</Button>
+					<Button
+						margin="0px 20px 0px 20px"
+						width="150px"
+						height="auto"
+						padding={['10px', '13px', '15px']}
+						_hover={{ background: bg, color: color }}
+					>
+						<Link className="link" isExternal href={gitHub}>
+							<Text
+								fontSize={['18px', '18px', '20px']}
+								fontWeight="bold"
+							>
+								Code<Icon as={FaCode} marginLeft="5px"></Icon>
+							</Text>
+						</Link>
+					</Button>
+				</CardFooter>
+			</Card>
+		</Zoom>
 	);
 };
