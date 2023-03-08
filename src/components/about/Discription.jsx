183071f71dfdad510c653ff8ef90b4f6d15f388c
avinashkvar <k3076183@gmail.com>
2023-03-08 09:16:01 +0530

    skills

--- src/components/about/Discription.jsx
+++ src/components/about/Discription.jsx
@@ -4,7 +4,7 @@ export const Description = () => {
 	return (
 		<Text fontSize="18px">
 			Passionate <b>Full Stack Web Developer</b> Many things excite me in
-			this expanse of technology, Currently Working as{' '}
+			this expanse of technology, Worked as{' '}
 			<b>Instructional Associate at Masai School</b>, and I like to explore
 			and keep my options open all the time. Apart from technical things, I
 			enjoy playing cricket and traveling. Now you know me a bit, please
